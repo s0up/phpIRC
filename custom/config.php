6cee41a1488e7a779a18b0615d359245f5fd4fe1
s0up <mike@haxt.net>
2011-06-20 23:22:58 -0700

    Edited custom/config.php via GitHub

--- custom/config.php
+++ custom/config.php
@@ -2,7 +2,7 @@
 define(IRC_HOST, 'irc.freenode.net');
 define(IRC_PORT, '8001');
 define(IRC_USERNAME, 'nick');
-define(IRC_PASSWORD, 'password'');
+define(IRC_PASSWORD, 'password');
 define(IRC_NICK, 'newbot001');
 define(IRC_CHANNEL, '#channel');
 define(IRC_FULL_NAME, 'John Doe');
